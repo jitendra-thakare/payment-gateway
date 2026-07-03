63a2b037558d57eae58de975d6bf50183cd59e49
Jitendra <jitendrathakare2000@gmail.com>
2026-07-03 13:08:06 +0530

    commit-1-spring-initializer

---
+++ src/test/java/com/way2paid/payment_gateway/PaymentGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.way2paid.payment_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
