b0a08146c86142a169aebc5aa85f89dd6c098e96
Jitendra <jitendrathakare2000@gmail.com>
2026-07-05 17:06:37 +0530

    commit-7-creating-appUser-entity

---
+++ src/main/java/com/way2paid/payment_gateway/merchant/entity/AppUser.java
@@ -0,0 +1,28 @@
+package com.way2paid.payment_gateway.merchant.entity;
+
+import com.way2paid.payment_gateway.common.enums.UserRole;
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "app_user")
+public class AppUser {
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "merchant_id")
+    private Merchant merchant;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole role;
+}
