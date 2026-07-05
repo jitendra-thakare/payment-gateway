860eb1780030a3fb465acee57acf0e47fcbfa6e9
Jitendra <jitendrathakare2000@gmail.com>
2026-07-05 18:55:57 +0530

    commit-13-creating-payment-transition-entity

---
+++ src/main/java/com/way2paid/payment_gateway/payment/PaymentTransitionLog.java
@@ -0,0 +1,37 @@
+package com.way2paid.payment_gateway.payment;
+
+import com.way2paid.payment_gateway.common.enums.PaymentEvent;
+import com.way2paid.payment_gateway.common.enums.PaymentStatus;
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "payment_transition_log")
+public class PaymentTransitionLog {
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "payment_id", nullable = false)
+    private Payment payment;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "from_status", nullable = false, length = 20)
+    private PaymentStatus fromStatus;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name="event", length = 20,nullable = false)
+    private PaymentEvent event;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "to_status", nullable = false, length = 30)
+    private PaymentStatus toStatus;
+
+    @Column(name="actor", length = 100)
+    private String actor;
+
+    @Column(name="occurred_at", nullable = false)
+    private java.time.LocalDateTime occurredAt;
+}
