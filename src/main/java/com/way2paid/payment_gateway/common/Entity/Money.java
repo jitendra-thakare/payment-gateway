f0af8fd9e48954b1d4e082a69e9e0b12033ace97
Jitendra <jitendrathakare2000@gmail.com>
2026-07-05 18:08:10 +0530

    commit-8-creating-common-money-entity

---
+++ src/main/java/com/way2paid/payment_gateway/common/Entity/Money.java
@@ -0,0 +1,37 @@
+package com.way2paid.payment_gateway.common.Entity;
+
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class Money {
+    private int amountUnits;
+    public String currency;
+
+    public Money(int amountUnits, String currency) {
+        this.amountUnits = amountUnits;
+        this.currency = currency;
+    }
+
+    public Money of(int amountUnits, String currency) {
+        return new Money(amountUnits, currency);
+    }
+
+    private Money inr(int amountUnits) {
+        return new Money(amountUnits, "INR");
+    }
+
+    public Money add(Money other) {
+        if(!this.currency.equals(other.currency)) {
+            throw new IllegalArgumentException("Cannot add Money with different currencies");
+        }
+        return new Money(this.amountUnits + other.amountUnits, this.currency);
+    }
+
+    public Money subtract(Money other) {
+        if(!this.currency.equals(other.currency)) {
+            throw new IllegalArgumentException("Cannot subtract Money with different currencies");
+        }
+        return new Money(this.amountUnits - other.amountUnits, this.currency);
+    }
+}
