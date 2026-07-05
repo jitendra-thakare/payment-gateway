2660f03987d3c920976baf02b7d722ee389eab3f
Jitendra <jitendrathakare2000@gmail.com>
2026-07-05 12:34:28 +0530

    commit-4-creating-enums

---
+++ src/main/java/com/way2paid/payment_gateway/common/enums/BusinessType.java
@@ -0,0 +1,10 @@
+package com.way2paid.payment_gateway.common.enums;
+
+public enum BusinessType {
+    LLP,
+    PROPRIETORSHIP,
+    PRIVATE_LIMITED,
+    PUBLIC_LIMITED,
+    PARTNERSHIP,
+    TRUST,
+}
---
+++ src/main/java/com/way2paid/payment_gateway/common/enums/MerchantStatus.java
@@ -0,0 +1,7 @@
+package com.way2paid.payment_gateway.common.enums;
+
+public enum MerchantStatus {
+    PENDING_KYC,
+    ACTIVE,
+    SUSPENDED,
+}
