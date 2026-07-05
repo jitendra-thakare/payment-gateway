3e4f71501cc9aea211e5795d8ce382be39e7052b
Jitendra <jitendrathakare2000@gmail.com>
2026-07-05 18:18:49 +0530

    commit-10-creating-order-record-entity

---
+++ src/main/java/com/way2paid/payment_gateway/common/enums/OrderStatus.java
@@ -0,0 +1,8 @@
+package com.way2paid.payment_gateway.common.enums;
+
+public enum OrderStatus {
+    CREATED,
+    PAID,
+    ATTEMPTED,
+    CANCELLED,
+}
--- src/main/java/com/way2paid/payment_gateway/payment/OrderRecord.java
+++ src/main/java/com/way2paid/payment_gateway/payment/OrderRecord.java
@@ -1,8 +1,13 @@
 package com.way2paid.payment_gateway.payment;
 
 import com.way2paid.payment_gateway.common.Entity.Money;
+import com.way2paid.payment_gateway.common.enums.OrderStatus;
 import jakarta.persistence.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
+import java.time.LocalDateTime;
+import java.util.Map;
 import java.util.UUID;
 
 @Entity
@@ -19,5 +24,17 @@ public class OrderRecord {
     @Embedded
     private Money amount;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private OrderStatus orderStatus;
 
+    @Column(nullable = false)
+    private Integer attempts = 0;
+
+    @JdbcTypeCode((SqlTypes.JSON))
+    @Column(columnDefinition = "jsonb")
+    private Map<String, Object> notes;
+
+    @Column(nullable = false, name = "expires_at")
+    private LocalDateTime expiresAt;
 }
